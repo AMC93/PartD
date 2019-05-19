c5bfdc1c7b0b8bd2548a4fefdf1a4545e523ac49
AMC93 <48273658+AMC93@users.noreply.github.com>
2019-05-19 13:41:12 +1200

    Add files via upload

--- PartD.java
+++ PartD.java
@@ -4,6 +4,7 @@ public class PartD {
 	public static void main(String[] args) {
 		System.out.println("Hi, my name is Aidan.");	
 		System.out.println("My favourite colour is red.");	
+		System.out.println("My favourite mode of transportation is by plane.");	
 
 	}
 
